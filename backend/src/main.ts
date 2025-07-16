02d598a9c1a43bf3cad41e34dbeba18c847fd931
sanitaravel <alexander.koshcheev.msk@gmail.com>
2025-07-16 10:25:22 +0200

    feat: add function to retrieve local external IP and log server address on startup feat: update .gitignore to include .env file feat: create .env.example for frontend environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as os from 'os';
+
+function getLocalExternalIp() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]!) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.enableCors({
     origin: true, // or true for all origins
     credentials: true,
@@ -16,6 +30,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const server = await app.listen(port);
+  const address = server.address();
+  const ip = getLocalExternalIp();
+  console.log(`API is running at: http://${ip}:${address.port}/`);
 }
 bootstrap();
